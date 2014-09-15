f65296f3173406b206f8a4579d2caa26d9bd56c0
blaskxx <pablomadrigaless@gmail.com>
2014-09-15 00:31:54 -0600

    Merge terminado

--- src/sample/Controller/ControllerVistaPrincipal.java
+++ src/sample/Controller/ControllerVistaPrincipal.java
@@ -20,7 +20,9 @@ import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 
-public class Controller implements Initializable {
+public class ControllerVistaPrincipal implements Initializable, ControlledScreen {
+    ScreensController myController;
+
     @FXML   BorderPane mainPane;
    //chart
     @FXML   LineChart<String,Number> cpu_chart;
@@ -30,8 +32,8 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        ORCConnection connection = ORCConnection.Instance();
-        connection.initializeConnection("SYSTEM", "sysdba", "root", "192.168.1.111", "XE", 1521, false);
+        //ORCConnection connection = ORCConnection.Instance();
+        //connection.initializeConnection("SYSTEM", "sysdba", "root", "192.168.1.111", "XE", 1521, false);
         cpu_chart.setData(CpuTimeSeries.getInstance().getCpu_use());
         cpu_chart.getYAxis().setAutoRanging(false);
 
@@ -42,5 +44,10 @@ public class Controller implements Initializable {
     @FXML void stupid(){
         for( XYChart.Data<String, Number> i:cpu_chart.getData().get(0).getData()) System.out.print(", "+i.getXValue()+":"+i.getYValue());
         System.out.println();
+    }
+
+    @Override
+    public void setScreenParent(ScreensController screenPage) {
+        screenPage=myController;
     }
 }
