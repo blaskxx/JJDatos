c7389adf7250b4fa1bd72cae613b8bbd1406a0d2
blaskxx <pablomadrigaless@gmail.com>
2014-09-15 00:20:17 -0600

    Grafico_1

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,17 +2,34 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sample.Controller.ScreensController;
 
 public class Main extends Application {
 
+    public static String screen1ID = "main";
+    public static String screen1File = "../View/Login.fxml";
+    public static String screen2ID = "screen2";
+    public static String screen2File = "../View/VistaPrincipal.fxml";
+
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("View/VistaPrincipal.fxml"));
-        primaryStage.setTitle("Monitor");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        ScreensController mainContainer = new ScreensController();
+        mainContainer.loadScreen(Main.screen1ID, Main.screen1File);
+        mainContainer.loadScreen(Main.screen2ID, Main.screen2File);
+
+        mainContainer.setScreen(Main.screen1ID);
+
+        Group root = new Group();
+        root.getChildren().addAll(mainContainer);
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setMinWidth(900);
+        primaryStage.setMinHeight(450);
         primaryStage.show();
     }
 
